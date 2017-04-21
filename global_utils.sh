eb8b0a6c738d75cbd9d79c7a19ea7e71b80e7be5
dylanjay710 <dylanjay710@gmail.com>
2017-04-21 02:55:22 -0400

    added updated global_utils.sh

---
+++ global_utils.sh
@@ -0,0 +1,16 @@
+gem_path='/var/lib/gems/2.4.0/gems/'
+
+# emacs -nw [filename]
+function enw {
+    filename=$1
+    emacs -nw $filename
+}
+
+#start 
+function srb {
+    bash ~/Desktop/RubyMine-2017.1.1/bin/rubymine.sh
+}
+
+
+
+
