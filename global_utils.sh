fe8bd46cf9d23cbf3d63c9f3cd61bde0541c9d0e
dylanjay710 <dylanjay710@gmail.com>
2017-04-21 04:30:57 -0400

    added empty file global_utils.sh for the rubystack-1-vm compute engine instance

--- global_utils.sh
+++ global_utils.sh
@@ -1,21 +1,2 @@
-gem_path='/var/lib/gems/2.4.0/gems/'
 
-# emacs -nw [filename]
-function enw {
-    filename=$1
-    emacs -nw $filename
-}
-
-# start ruby mine
-function srb {
-    bash ~/Desktop/RubyMine-2017.1.1/bin/rubymine.sh
-}
-
-# start android studio
-function sas {
-	bash ~/Desktop/android-studio/bin/studio.sh
-}
-
-
-
-
+# bash helper functions for daftware.io ruby stack
